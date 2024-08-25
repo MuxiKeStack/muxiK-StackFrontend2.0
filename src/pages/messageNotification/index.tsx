0a60ba54a9dfc9159d35e1ccb325f5e7121a2c54
墨綠BG <s5460703@gmail.com>
2024-08-25 14:50:45 +0800

    🔨 refactor：样式重构

--- src/pages/messageNotification/index.tsx
+++ src/pages/messageNotification/index.tsx
@@ -28,7 +28,7 @@ const MessageNotification: React.FC<MessageNotificationProps> = memo(() => {
   });
 
   return (
-    <View className="MessageNotification">
+    <View className="flex h-[95vh] w-full flex-col items-center gap-4 overflow-y-scroll pt-2">
       <Message
         username={notification.username}
         eventType={notification.eventType}
@@ -48,22 +48,22 @@ const MessageNotification: React.FC<MessageNotificationProps> = memo(() => {
 const Message: React.FC<MessageProps> = memo(
   ({ username, eventType, description, comment }) => {
     return (
-      <View className="messageNotification_message">
-        <View className="messageNotification_photo"></View>
-        <View className="messageNotification_detail">
-          <View className="messageNotification_username">{username}</View>
-          <View className="messageNotification_event">
-            <View className="messageNotification_event_type">
+      <View className="flex w-[90%] gap-4">
+        <View className="h-[8vh] w-[8vh] rounded-full border bg-[#f9f9f2]"></View>
+        <View className="flex flex-col gap-2">
+          <View className="text-sm font-bold">{username}</View>
+          <View className="flex">
+            <View className="text-sm text-gray-300">
               {eventType ? '回复：' : '赞了我'}
             </View>
-            {eventType && (
-              <View className="messageNotification_description">{description}</View>
-            )}
+            {eventType && <View className="text-sm text-gray-500">{description}</View>}
           </View>
-          <View className="messageNotification_comment">{comment}</View>
+          <View className="border-l-4 border-gray-300 pl-2 text-sm text-gray-500">
+            {comment}
+          </View>
           {eventType && (
-            <View className="messageNotification_reply">
-              <View className="messageNotification_reply_text">回复TA:</View>
+            <View className="flex h-6 w-full items-center rounded-lg bg-[#f9f9f2] px-2">
+              <View className="text-xs text-gray-300">回复TA:</View>
             </View>
           )}
         </View>
