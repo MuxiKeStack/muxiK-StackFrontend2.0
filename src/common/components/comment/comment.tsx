bd18214c5642bcbb2b388c34239b59e63d10cc01
eleliauk <2831336720@qq.com>
2024-08-26 02:44:56 +0800

    🐞 fix：修复登陆路由

--- src/common/components/comment/comment.tsx
+++ src/common/components/comment/comment.tsx
@@ -34,7 +34,15 @@ export default function Comment(props: CommentInfo & { type?: string }) {
       useCourseStore.getState().getPublishers(publisher_id || 0);
     });
   }, [course_id, publisher_id]);
+  async function navigateToPage() {
+    await Taro.navigateTo({
+      url: '/pages/classInfo/index', // 确保路径正确
+    });
+  }
 
+  function handleClickToClass() {
+    navigateToPage().then((r) => console.log(r)); // 这里调用异步函数，但不返回 Promise
+  }
   return (
     <View className="bigcomment" onClick={handleClick}>
       <View className="commentplus">
@@ -44,7 +52,7 @@ export default function Comment(props: CommentInfo & { type?: string }) {
           </>
         ) : (
           <>
-            <View className="classTitle">
+            <View className="classTitle" onClick={handleClickToClass}>
               {courseDetail[course_id || 0]?.name +
                 ' (' +
                 courseDetail[course_id || 0]?.teacher +
