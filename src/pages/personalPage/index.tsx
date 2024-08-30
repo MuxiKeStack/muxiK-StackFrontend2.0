d6ec5bebef6f35dd7a24a977e8abbc5155eaf95a
墨綠BG <s5460703@gmail.com>
2024-08-31 01:52:05 +0800

    🔨 refactor：重构个人页

--- src/pages/personalPage/index.tsx
+++ src/pages/personalPage/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable import/first */
 import { Image, Progress, View } from '@tarojs/components';
-import Taro, { useLoad } from '@tarojs/taro';
-import React, { useEffect, useState } from 'react';
+import Taro from '@tarojs/taro';
+import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { AtList, AtListItem } from 'taro-ui';
 
 import './index.scss';
@@ -16,6 +16,7 @@ import {
   TopBackground,
 } from '@/common/assets/img/personalPage';
 import TitleButton from '@/common/components/titleButton/titleButton';
+import uniqueKeyUtil from '@/common/utils/keyGen';
 
 export interface UserInfo {
   avatarUrl: string; // 用户头像的URL
@@ -52,98 +53,94 @@ export interface WebUserProfileVo {
   using_title: string;
   utime?: number;
 }
-const PersonalPage: React.FC<PersonalPageProps> = () => {
-  useLoad(() => {
-    console.log('Page loaded.');
+
+const ListItems: { title: string; icon: string; url: string }[] = [
+  { title: '我的课程', icon: BookIcon, url: '/pages/myclass/myclass' },
+  { title: '我的收藏', icon: StarIcon, url: '/pages/myCollection/index' },
+  {
+    title: '评课历史',
+    icon: ClockIcon,
+    url: '/subpackages/profile/pages/history/index',
+  },
+  { title: '意见反馈', icon: MailIcon, url: '/pages/feedback/index' },
+];
+
+const Page: React.FC<PersonalPageProps> = memo(() => (
+  <View className="PersonalPage">
+    <Head />
+    <List />
+  </View>
+));
+
+const Head: React.FC = memo(() => {
+  const [user, setUser] = useState<{
+    level: number;
+    points: number;
+    nextLevel: number;
+    avatarUrl: string;
+    nickName: string;
+    selectedTitle: string;
+    newUser: boolean;
+  }>({
+    level: 1,
+    points: 0,
+    nextLevel: 0,
+    avatarUrl: '',
+    nickName: '昵称昵称昵称',
+    selectedTitle: 'None',
+    newUser: false,
   });
 
-  return (
-    <View className="PersonalPage">
-      <Head />
-      <List />
-    </View>
-  );
-};
+  const translateTitle = useMemo(() => {
+    const titleMap = {
+      CaringSenior: '知心学长',
+      KeStackPartner: '课栈合伙人',
+      CCNUWithMe: '华师有我',
+    };
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return (title: string) => titleMap[title] || title;
+  }, []);
 
-const Head = () => {
-  const [level, setLevel] = useState(1);
-  const [nextLevel, setNextLevel] = useState(0);
-  const [points, setPoints] = useState(0);
-  const [avatarUrl, setAvatarUrl] = useState('');
-  const [nickName, setNickName] = useState('昵称昵称昵称');
-  const [selectedTitle, setSelectedTitle] = useState<string>('None');
-  const [newUser, setNewUser] = useState(false);
   useEffect(() => {
-    const fetchExp = async () => {
+    const fetchUserData = async () => {
       try {
-        const url = '/points/users/mine';
-        const response: ResponseLevel = await get(url);
-        console.log(response);
-        setLevel(response.data.level);
-        setPoints(response.data.points);
-        setNextLevel(response.data.next_level_points);
+        const responseLevel: ResponseLevel = await get('/points/users/mine');
+        const responseUser: ResponseUser = await get('/users/profile');
+        setUser({
+          level: responseLevel.data.level,
+          points: responseLevel.data.points,
+          nextLevel: responseLevel.data.next_level_points,
+          avatarUrl: responseUser.data.avatar,
+          nickName: responseUser.data.nickname,
+          selectedTitle: responseUser.data.using_title
+            ? translateTitle(responseUser.data.using_title)
+            : 'None',
+          newUser: responseUser.data.new,
+        });
       } catch (error) {
         console.error('Error fetching collection data:', error);
       }
     };
-    void fetchExp();
-  }, []);
-  useEffect(() => {
-    const fetchNew = async () => {
-      try {
-        const url = '/users/profile';
-        const response: ResponseUser = await get(url);
-        console.log('用户信息');
-        console.log(response);
-        setNewUser(response.data.new);
-        setNickName(response.data.nickname);
-        setAvatarUrl(response.data.avatar);
-        console.log(response.data.using_title);
-        let translatedTitle = '';
-        switch (response.data.using_title) {
-          case 'CaringSenior':
-            translatedTitle = '知心学长';
-            setSelectedTitle('知心学长');
-            break;
-          case 'KeStackPartner':
-            translatedTitle = '课栈合伙人';
-            setSelectedTitle('课栈合伙人');
-            break;
-          case 'CCNUWithMe':
-            translatedTitle = '华师有我';
-            setSelectedTitle('华师有我');
-            break;
-          default:
-            translatedTitle = response.data.using_title;
-            break;
-        }
-        setSelectedTitle(translatedTitle);
-        console.log(translatedTitle);
-        console.log(selectedTitle);
-      } catch (error) {
-        console.error('Error fetching collection data:', error);
-      }
-    };
-    void fetchNew();
-  }, []);
-  if (newUser) {
+    void fetchUserData();
+  }, [translateTitle]);
+
+  if (user.newUser) {
     void Taro.navigateTo({ url: '/pages/editUser/index' });
   }
 
   return (
     <>
-      {}
       <Image src={TopBackground} className="personalPage_top_background"></Image>
       <View className="personalPage_user_container">
         <View className="personalPage_user_photo">
-          <Image src={avatarUrl} className="personalPage_user_photo" />
+          <Image src={user.avatarUrl} className="personalPage_user_photo" />
         </View>
         <View className="personalPage_user_container1">
           <View className="personalPage_user_details">
-            <View className="personalPage_username">{nickName}</View>
+            <View className="personalPage_username">{user.nickName}</View>
             <View className="personalPage_username_title">
-              {selectedTitle !== 'None' && (
-                <TitleButton title={selectedTitle}></TitleButton>
+              {user.selectedTitle !== 'None' && (
+                <TitleButton title={user.selectedTitle}></TitleButton>
               )}
             </View>
             <View
@@ -157,12 +154,12 @@ const Head = () => {
           </View>
           {/* 经验 */}
           <View className="personalPage_exp_value">
-            {points}/{nextLevel}
+            {user.points}/{user.nextLevel}
           </View>
           <View className="personalPage_progress_line">
-            <View className="personalPage_exp_text">Exp{level}&nbsp;&nbsp;</View>
+            <View className="personalPage_exp_text">Exp{user.level}&nbsp;&nbsp;</View>
             <Progress
-              percent={(points / nextLevel) * 100}
+              percent={(user.points / user.nextLevel) * 100}
               color="orange"
               strokeWidth={6}
               borderRadius={100}
@@ -173,45 +170,24 @@ const Head = () => {
       </View>
     </>
   );
-};
+});
 
-const List = () => {
+const List: React.FC = memo(() => {
+  const navigate = useCallback((url: string) => void Taro.navigateTo({ url }), []);
+
   return (
     <AtList className="personalPage_list">
-      <AtListItem
-        title="我的课程"
-        arrow="right"
-        thumb={BookIcon}
-        onClick={() => {
-          void Taro.navigateTo({ url: '/pages/myclass/myclass' });
-        }}
-      />
-      <AtListItem
-        title="我的收藏"
-        arrow="right"
-        thumb={StarIcon}
-        onClick={() => {
-          void Taro.navigateTo({ url: '/pages/myCollection/index' });
-        }}
-      />
-      <AtListItem
-        title="评课历史"
-        arrow="right"
-        thumb={ClockIcon}
-        onClick={() => {
-          void Taro.navigateTo({ url: '/subpackages/profile/pages/history/index' });
-        }}
-      />
-      <AtListItem
-        title="意见反馈"
-        arrow="right"
-        thumb={MailIcon}
-        onClick={() => {
-          void Taro.navigateTo({ url: '/pages/feedback/index' });
-        }}
-      />
+      {ListItems.map((item) => (
+        <AtListItem
+          key={uniqueKeyUtil.nextKey()}
+          title={item.title}
+          arrow="right"
+          thumb={item.icon}
+          onClick={() => navigate(item.url)}
+        />
+      ))}
     </AtList>
   );
-};
+});
 
-export default PersonalPage;
+export default Page;
