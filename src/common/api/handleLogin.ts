c688344caf317b927ea78c8b35bf60b1cb810b80
墨綠BG <s5460703@gmail.com>
2024-08-31 03:46:12 +0800

    🐞 fix：移除部份 ESLint 警告

--- src/common/api/handleLogin.ts
+++ src/common/api/handleLogin.ts
@@ -2,25 +2,15 @@ import Taro from '@tarojs/taro';
 
 const preUrl = 'https://kstack.muxixyz.com';
 
-interface LoginResponseHeaders {
+type LoginResponseHeaders = {
   'X-Jwt-Token'?: string;
   'X-Refresh-Token'?: string;
-}
+};
 
 const handleLogin = async (data: Record<string, unknown> = {}) => {
   const header = {
     'Content-Type': 'application/json;charset=utf-8',
   };
-
-  await Taro.setStorage({
-    key: 'shortToken',
-    data: 'shortToken',
-  });
-
-  await Taro.setStorage({
-    key: 'longToken',
-    data: 'longToken',
-  });
 
   try {
     const response = await Taro.request({
@@ -35,46 +25,38 @@ const handleLogin = async (data: Record<string, unknown> = {}) => {
     const longToken = headers['X-Refresh-Token'];
 
     if (shortToken && longToken) {
-      await Taro.setStorage({
+      void Taro.setStorage({
         key: 'shortToken',
         data: shortToken.toString(),
-        success: () => {
-          console.log('shortToken 设置成功');
-          // 方便看情况 log 出 shortToken 后期上线之前删除掉这个
-          console.log(shortToken);
-        },
       });
 
-      await Taro.setStorage({
+      void Taro.setStorage({
         key: 'longToken',
         data: longToken.toString(),
-        success: () => {
-          console.log('longToken 设置成功');
-          // 方便看情况 log 出 longToken 后期上线之前删除掉这个
-          console.log(longToken);
-        },
       });
 
-      await Taro.switchTab({
+      void Taro.switchTab({
         url: '/pages/main/index',
       });
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (response.data.code !== 0) {
-      console.log('登陆失败(code 不为 0)');
-      await Taro.showToast({
+      // 登陆失败(code 不为 0)
+      void Taro.showToast({
         icon: 'error',
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-        title: response.data.msg,
+        title: '登录失败，请重试',
       });
     }
 
     if (!response.statusCode.toString().startsWith('2')) {
-      throw new Error(`${response.statusCode}`);
+      throw new Error(`请求失败，状态码：${response.statusCode}`);
     }
   } catch (error) {
-    console.log(error);
+    void Taro.showToast({
+      icon: 'error',
+      title: (error as Error).message || '登录过程中发生错误',
+    });
   }
 };
 
