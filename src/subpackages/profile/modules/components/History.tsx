361ac0321df9e37c5ef5b1ba6076dc7ce9134291
墨綠BG <s5460703@gmail.com>
2024-09-01 23:43:27 +0800

    :poop: poop：新增评课历史内容

--- src/subpackages/profile/modules/components/History.tsx
+++ src/subpackages/profile/modules/components/History.tsx
@@ -1,63 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { View } from '@tarojs/components';
 import Taro from '@tarojs/taro';
-import React, { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 
+import { CommentInfo } from '@/common/assets/types';
 import { Comment } from '@/common/components';
+import { get } from '@/common/utils/fetch';
 import uniqueKeyUtil from '@/common/utils/keyGen';
 
 const History: React.FC = memo(() => {
+  const [comments, setComments] = useState<CommentInfo[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await get(
+        `evaluations/list/mine?cur_evaluation_id=${0}&limit=${10}&status=${'Public'}`
+      );
+      console.log(res.data);
+      setComments(res.data);
+    };
+    void fetchData();
+  }, []);
+
   return (
     <View className="flex h-screen flex-col items-center gap-4 overflow-y-scroll py-4">
-      <Comment
-        onClick={(props) => {
-          const serializedComment = encodeURIComponent(JSON.stringify(props));
-          void Taro.navigateTo({
-            url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
-          });
-        }}
-        key={uniqueKeyUtil.nextKey()}
-        type="inner"
-      />
-      <Comment
-        onClick={(props) => {
-          const serializedComment = encodeURIComponent(JSON.stringify(props));
-          void Taro.navigateTo({
-            url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
-          });
-        }}
-        key={uniqueKeyUtil.nextKey()}
-        type="inner"
-      />
-      <Comment
-        onClick={(props) => {
-          const serializedComment = encodeURIComponent(JSON.stringify(props));
-          void Taro.navigateTo({
-            url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
-          });
-        }}
-        key={uniqueKeyUtil.nextKey()}
-        type="inner"
-      />
-      <Comment
-        onClick={(props) => {
-          const serializedComment = encodeURIComponent(JSON.stringify(props));
-          void Taro.navigateTo({
-            url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
-          });
-        }}
-        key={uniqueKeyUtil.nextKey()}
-        type="inner"
-      />
-      <Comment
-        onClick={(props) => {
-          const serializedComment = encodeURIComponent(JSON.stringify(props));
-          void Taro.navigateTo({
-            url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
-          });
-        }}
-        key={uniqueKeyUtil.nextKey()}
-        type="inner"
-      />
+      {comments.map((item) => (
+        <Comment
+          key={uniqueKeyUtil.nextKey()}
+          type="inner"
+          {...item}
+          onClick={(props) => {
+            const serializedComment = encodeURIComponent(JSON.stringify(props));
+            void Taro.navigateTo({
+              url: `/pages/evaluateInfo/index?comment=${serializedComment}`,
+            });
+          }}
+        />
+      ))}
     </View>
   );
 });
