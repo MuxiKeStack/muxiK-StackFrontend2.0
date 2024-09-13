f59a7b6cef94358879b50672c57b39943aa9fb55
墨綠BG <s5460703@gmail.com>
2024-09-13 23:58:42 +0800

    ✨ feat：新增 Contact 组件

--- src/modules/guide/components/Guide.tsx
+++ src/modules/guide/components/Guide.tsx
@@ -1,6 +1,7 @@
 import { View } from '@tarojs/components';
 import { memo } from 'react';
 
+import Contact from './Contact';
 import Selector from './Selector';
 import Source from './Source';
 
@@ -8,6 +9,7 @@ const Guide: React.FC = memo(() => (
   <View className="flex h-screen w-full flex-col items-center gap-4 overflow-y-scroll px-4 pb-[13vh] pt-2">
     <Selector />
     <Source />
+    <Contact />
   </View>
 ));
 
