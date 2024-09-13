52c6c511ac68033b8afbd3da4eff543e946cdee6
墨綠BG <s5460703@gmail.com>
2024-09-13 23:49:23 +0800

    ✨ feat：新增 Source 组件

--- src/modules/guide/components/Guide.tsx
+++ src/modules/guide/components/Guide.tsx
@@ -2,13 +2,13 @@ import { View } from '@tarojs/components';
 import { memo } from 'react';
 
 import Selector from './Selector';
+import Source from './Source';
 
-const Guide: React.FC = memo(() => {
-  return (
-    <View className="flex h-screen w-full flex-col items-center gap-4 overflow-y-scroll px-4 pb-[13vh] pt-2">
-      <Selector />
-    </View>
-  );
-});
+const Guide: React.FC = memo(() => (
+  <View className="flex h-screen w-full flex-col items-center gap-4 overflow-y-scroll px-4 pb-[13vh] pt-2">
+    <Selector />
+    <Source />
+  </View>
+));
 
 export default Guide;
---
+++ src/modules/guide/components/Source.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from '@tarojs/components';
+import { memo } from 'react';
+import { AtIcon } from 'taro-ui';
+
+interface SourceProps {}
+
+const SourceItem: React.FC<{ text: string }> = memo(({ text }) => (
+  <View className="flex w-full items-center gap-4 p-2 hover:bg-gray-200">
+    <AtIcon value="file-generic" size="35" color="#f18900" />
+    <Text className="text-sm">{text}</Text>
+  </View>
+));
+
+const Source: React.FC<SourceProps> = memo(() => {
+  return (
+    <View className="h-auto min-h-[75vh] w-full rounded-lg bg-[#f9f9f2] px-4 py-2">
+      <SourceItem text="2022-2023第一学期选课手册" />
+      <SourceItem text="2022-2023第一学期选课手册(2)" />
+      <SourceItem text="2022-2023第一学期选课手册(3)" />
+    </View>
+  );
+});
+
+export default Source;
