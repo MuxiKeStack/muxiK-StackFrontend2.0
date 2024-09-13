07669578387e4ccf1dcf63de0b9f58503f066fc5
墨綠BG <s5460703@gmail.com>
2024-09-13 22:30:00 +0800

    ✨ feat：新增选课手册页面

---
+++ src/pages/guide/index.config.ts
@@ -0,0 +1,3 @@
+export default definePageConfig({
+  navigationBarTitleText: '选课手册',
+});
---
+++ src/pages/guide/index.tsx
@@ -0,0 +1,9 @@
+import { memo } from 'react';
+
+import './index.scss';
+
+import Notification from '../../modules/notification';
+
+const Page: React.FC = memo(() => <Notification />);
+
+export default Page;
