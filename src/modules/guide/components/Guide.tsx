ce27f9edbcb11a31f5d0b47615e3b12c013fd7a3
墨綠BG <s5460703@gmail.com>
2024-09-13 23:07:58 +0800

    ✨ feat：新增 Selector 组件

--- src/modules/guide/components/Guide.tsx
+++ src/modules/guide/components/Guide.tsx
@@ -1,10 +1,12 @@
 import { View } from '@tarojs/components';
 import { memo } from 'react';
 
+import Selector from './Selector';
+
 const Guide: React.FC = memo(() => {
   return (
     <View className="flex h-screen w-full flex-col items-center gap-4 overflow-y-scroll px-4 pb-[13vh] pt-2">
-      123
+      <Selector />
     </View>
   );
 });
