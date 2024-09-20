d326bb2bb4a81863534175bd69985a83bcc9b91e
eleliauk <2831336720@qq.com>
2024-09-20 09:00:01 +0800

    ✨ feat：新增问问同学接口

--- src/common/components/AnswerToStudent/index.tsx
+++ src/common/components/AnswerToStudent/index.tsx
@@ -4,15 +4,22 @@ import './index.scss';
 
 // eslint-disable-next-line import/first
 import Ask from '@/common/assets/img/publishQuestion/img.png';
+// eslint-disable-next-line import/first
+import { Answerv1Answer } from '@/common/types/userTypes';
 
-function AnswerToStudent() {
+interface Question {
+  content: string;
+  preview_answers: Array<Answerv1Answer>;
+}
+const AnswerToStudent: React.FC<Question> = (props) => {
+  const { content, preview_answers } = props;
   return (
     <View className="question-container">
       {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
       <Image src={Ask} className="img"></Image>
-      <View>问题问题问题问题问题:</View>
-      <View>1个回答</View>
+      <View>{content}</View>
+      <View>{preview_answers.length}个回答</View>
     </View>
   );
-}
+};
 export default AnswerToStudent;
--- src/common/types/userTypes.ts
+++ src/common/types/userTypes.ts
@@ -63,3 +63,50 @@ export interface Grade {
   percent?: number;
   total_grades: number[];
 }
+/**
+ * ginx.Result
+ */
+export interface ResponseQuestion {
+  /**
+   * 错误码，非 0 表示失败
+   */
+  code?: number;
+  data: WebQuestionVo[];
+  /**
+   * 错误或成功 描述
+   */
+  msg?: string;
+}
+
+/**
+ * web.QuestionVo
+ */
+export interface WebQuestionVo {
+  answer_cnt?: number;
+  /**
+   * 具体针对那种业务的提问，如 Course
+   */
+  biz?: string;
+  biz_id?: number;
+  content: string;
+  ctime?: number;
+  id?: number;
+  preview_answers: Answerv1Answer[];
+  /**
+   * 提问者用户id
+   */
+  questioner_id?: number;
+  utime?: number;
+}
+
+/**
+ * answerv1.Answer
+ */
+export interface Answerv1Answer {
+  content?: string;
+  ctime?: number;
+  id?: number;
+  publisher_id?: number;
+  question_id?: number;
+  utime?: number;
+}
