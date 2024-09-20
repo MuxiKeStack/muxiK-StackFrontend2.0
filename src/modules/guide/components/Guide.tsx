7fa2c2a5cc167860175a76461ea263f4e705dc5c
墨綠BG <s5460703@gmail.com>
2024-09-21 00:11:09 +0800

    ✨ feat：新增弹层选择器组件

--- src/modules/guide/components/Guide.tsx
+++ src/modules/guide/components/Guide.tsx
@@ -7,9 +7,10 @@ import Source from './Source';
 
 const Guide: React.FC = memo(() => (
   <View className="flex h-screen w-full flex-col items-center gap-4 overflow-y-scroll px-4 pb-[13vh] pt-2">
-    <Selector />
-    <Source />
-    <Contact />
+    <Selector>
+      <Source />
+      <Contact />
+    </Selector>
   </View>
 ));
 
--- src/modules/guide/components/Selector.tsx
+++ src/modules/guide/components/Selector.tsx
@@ -1,27 +1,75 @@
+import { Picker, Popup } from '@taroify/core';
 import { ArrowDown } from '@taroify/icons';
 import { Text, View } from '@tarojs/components';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
-interface SelectorProps {}
+interface SelectorProps {
+  children: React.ReactNode;
+}
 
-const Selector: React.FC<SelectorProps> = memo(() => {
+const times = [
+  [
+    { label: '全部', value: '全部' },
+    { label: '2022', value: '2022' },
+    { label: '2023', value: '2023' },
+    { label: '2024', value: '2024' },
+    { label: '2025', value: '2025' },
+    { label: '2026', value: '2026' },
+  ],
+  [
+    { label: '全部', value: '全部' },
+    { label: '第一学期', value: '第一学期' },
+    { label: '第二学期', value: '第二学期' },
+    { label: '第三学期', value: '第三学期' },
+  ],
+];
+
+const Selector: React.FC<SelectorProps> = memo(({ children }) => {
+  const [selection, setSelection] = useState<{ year: string; term: string }>({
+    year: '全部',
+    term: '全部',
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <View className="flex w-full items-center justify-between px-2">
-      <View className="flex w-1/3 flex-col gap-2">
-        <Text className="pl-2 text-xs">选择学年</Text>
-        <View className="flex w-full justify-between rounded-lg bg-[#f9f9f2] px-2 py-1">
-          <Text className="text-xs">全部</Text>
-          <ArrowDown className="text-xs" style={{ color: '#f18900' }} />
+    <>
+      <View className="flex w-full items-center justify-between px-2">
+        <View className="flex w-1/3 flex-col gap-2">
+          <Text className="pl-2 text-xs">选择学年</Text>
+          <View
+            className="flex w-full justify-between rounded-lg bg-[#f9f9f2] px-2 py-1"
+            onClick={() => setIsOpen(true)}
+          >
+            <Text className="text-xs">{selection.year}</Text>
+            <ArrowDown className="text-xs" style={{ color: '#f18900' }} />
+          </View>
+        </View>
+        <View className="flex w-1/3 flex-col gap-2">
+          <Text className="pl-2 text-xs">选择学期</Text>
+          <View
+            className="flex w-full justify-between rounded-lg bg-[#f9f9f2] px-2 py-1"
+            onClick={() => setIsOpen(true)}
+          >
+            <Text className="text-xs">{selection.term}</Text>
+            <ArrowDown className="text-xs" style={{ color: '#f18900' }} />
+          </View>
         </View>
       </View>
-      <View className="flex w-1/3 flex-col gap-2">
-        <Text className="pl-2 text-xs">选择学期</Text>
-        <View className="flex w-full justify-between rounded-lg bg-[#f9f9f2] px-2 py-1">
-          <Text className="text-xs">全部</Text>
-          <ArrowDown className="text-xs" style={{ color: '#f18900' }} />
-        </View>
-      </View>
-    </View>
+      {children}
+      <Popup open={isOpen} placement="bottom">
+        <Popup.Close />
+        <Picker
+          style={{ marginBottom: '15vh' }}
+          defaultValue={['全部', '全部']}
+          title="选择学年和学期"
+          columns={times}
+          onConfirm={(value) => {
+            setSelection({ year: value[0], term: value[1] });
+            setIsOpen(false);
+          }}
+        ></Picker>
+      </Popup>
+    </>
   );
 });
 
