fe1752b7d8df7818efc42361366efdbf19895b95
墨綠BG <s5460703@gmail.com>
2024-09-22 01:44:40 +0800

    🐞 fix：修复 ESLint 报错

--- src/modules/guide/components/Source.tsx
+++ src/modules/guide/components/Source.tsx
@@ -2,8 +2,6 @@ import { Text, View } from '@tarojs/components';
 import { memo } from 'react';
 import { AtIcon } from 'taro-ui';
 
-interface SourceProps {}
-
 const SourceItem: React.FC<{ text: string }> = memo(({ text }) => (
   <View className="flex w-full items-center gap-4 p-2 hover:bg-gray-200">
     <AtIcon value="file-generic" size="35" color="#f18900" />
@@ -11,7 +9,7 @@ const SourceItem: React.FC<{ text: string }> = memo(({ text }) => (
   </View>
 ));
 
-const Source: React.FC<SourceProps> = memo(() => {
+const Source: React.FC = memo(() => {
   return (
     <View className="h-auto min-h-[75vh] w-full rounded-lg bg-[#f9f9f2] px-4 py-2">
       <SourceItem text="2022-2023第一学期选课手册" />
