9ac1de0b9616be7abea8cacdf6e1c2f5a25f88da
BoogiePop <enkidu921@outlook.com>
2026-09-18 12:01:43 +0800

    fix(login): restore login session on cold start
    冷启动时 App.componentDidMount 里的 checkToken 用 getCurrentPages()
    判断页面栈，但 onLaunch 阶段栈为空，导致已登录用户停在登录页。
    改在登录页 onShow 里兜底恢复登录态。

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,26 @@
 import { View } from '@tarojs/components';
+import Taro, { useDidShow } from '@tarojs/taro';
 import { memo } from 'react';
 
 import './index.scss';
 
+import { hasStoredSession } from '@/common/auth/session';
+
 import { AuthForm } from './component';
 
-const Login: React.FC = memo(() => (
-  <View className="login_container">
-    <View className="login_background"></View>
-    <AuthForm />
-  </View>
-));
+const Login: React.FC = memo(() => {
+  useDidShow(() => {
+    if (hasStoredSession()) {
+      void Taro.switchTab({ url: '/pages/main/index' });
+    }
+  });
+
+  return (
+    <View className="login_container">
+      <View className="login_background"></View>
+      <AuthForm />
+    </View>
+  );
+});
 
 export default Login;
