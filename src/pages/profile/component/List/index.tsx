9525d288bfdb582262436a1c35d787504e2b182c
Zzy-07qi <2943914872@qq.com>
2026-09-22 16:22:04 +0800

    fix:修改profile图标

--- src/pages/profile/component/List/index.tsx
+++ src/pages/profile/component/List/index.tsx
@@ -4,13 +4,11 @@ import { AtList, AtListItem } from 'taro-ui';
 
 import './index.scss';
 
-import {
-  BookIcon,
-  ClockIcon,
-  ExitIcon,
-  FeedbackIcon,
-  StarIcon,
-} from '@/common/assets/img/profile';
+import CourseIcon from '@/common/assets/img/myHome/Course.svg';
+import CollectionIcon from '@/common/assets/img/myHome/Collection.svg';
+import HistoryIcon from '@/common/assets/img/myHome/History.svg';
+import FeedbackIcon from '@/common/assets/img/myHome/Feedback.svg';
+import ExitIcon from '@/common/assets/img/myHome/Log out.svg';
 import { Modal } from '@/common/components/Modal';
 import { ROUTES } from '@/common/constants/routes';
 import { uniqueKey } from '@/common/utils';
@@ -66,17 +64,17 @@ const ProfileList: React.FC = memo(() => {
     () => [
       {
         title: '我的课程',
-        icon: BookIcon as string,
+        icon: CourseIcon as string,
         onClick: () => navigate('/pages/myclass/index'),
       },
       {
         title: '我的收藏',
-        icon: StarIcon as string,
+        icon: CollectionIcon as string,
         onClick: () => navigate(ROUTES.profile.myCollection),
       },
       {
         title: '评课历史',
-        icon: ClockIcon as string,
+        icon: HistoryIcon as string,
         onClick: () => navigate(ROUTES.profile.evaluationHistory),
       },
       {
